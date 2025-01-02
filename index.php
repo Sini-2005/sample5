6be4143ecf794763efe0b2dfbb522b9e60eacd13
Sini-2005 <siniyafathimamr23bcs050@skasc.ac.in>
2025-01-02 11:48:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+    </style>
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-box">
+            <h1>Sign In</h1>
+            <form action="#" method="post">
+                <input type="text" name="email" placeholder="Email or mobile number" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit">Sign In</button>
+                <div class="remember-me">
+                    <input type="checkbox" name="remember" id="remember">
+                    <label for="remember">Remember me</label>
+                </div>
+                <a href="#">Forgot password?</a>
+                <a href="#">New to Netflix? Sign up now.</a>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
